6df8d26f08c568ba7e3fb237e04145eb67bddde5
Yasmin Miani <31292481+YMiani@users.noreply.github.com>
2021-12-28 21:06:08 -0300

    Add files via upload

---
+++ Enum/Genero.cs
@@ -0,0 +1,16 @@
+namespace DIO.Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Terror = 4,
+        Documentario = 5,
+        Fantasia = 6,
+        Ficcao = 7,
+        Musical = 8,
+        Romance = 9,
+        Drama = 10,
+    }
+}
